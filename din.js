26adb0b2053242f74c391382df65f3b4e12e2e94
Raadad Elsleiman <raadad@gmail.com>
2015-03-11 14:44:32 +1100

    removing prefixing

--- din.js
+++ din.js
@@ -2,52 +2,42 @@ module.exports = function(config) {
     var Din = function() {
         var self = this,
             fs = require('fs'),
-            prefixDir = config.baseDir ? config.baseDir : process.cwd(),
-
-
-            loader = {};
-        loader['x'] = function(name) { return require(name); };
-        loader['n'] = function(name) { return require(name); };
-        loader['d'] = function(name) { return self.load(name); };
-        loader['s'] = function(name) { return name; };
-        loader['j'] = function(name) { return config.evals[name](); };
+            prefixDir = config.baseDir ? config.baseDir : process.cwd();
 
         if (!config.singletons) config.singletons = {};
 
         // TODO: consider package.json entry point in this logic.
-        self.resolve = function(path) {
-            if (fs.existsSync(prefixDir + '/' + path)) {
-                return prefixDir + '/' + path;
-            }
+        self.resolve = function(path, previousPath) {
+            var pre = previousPath ? previousPath : prefixDir;
 
-            if (fs.existsSync(prefixDir + '/' + path + '.js')) {
-                return prefixDir + '/' + path + '.js';
-            }
+            if (fs.existsSync(pre + '/' + path)) return prefixDir + '/' + path;
+            if (fs.existsSync(pre + '/' + path + '.js')) return prefixDir + '/' + path + '.js';
+            if (fs.existsSync(pre + '/node_modules/' + path )) return prefixDir + '/node_modules/' + path;
+
+            if (previousPath) return self.resolve(path);
 
             return path;
         };
 
-        self.load = function load(alias) {
+        self.load = function load(alias, previousPath) {
+            if (typeof alias !== "string") return alias;
+            if (alias.indexOf('s:') !== -1) return alias.split(':')[1];
+
             var moduleDescriptor = config.graph[alias];
-            if (!moduleDescriptor) throw new Error('alias: ' + alias + ' not specified in configuration');
-
             if (config.singletons[alias]) return config.singletons[alias];
+            if (!moduleDescriptor) moduleDescriptor = {};
+            if (!moduleDescriptor.lookup) moduleDescriptor.lookup = alias;
 
-            var dependencies = moduleDescriptor.deps.map(function(item) {
-                    if (typeof item !== "string") return item;
-                    var dependencyParts = item.split(':'),
-                        dependencyType = dependencyParts[0],
-                        dependencyAlias = dependencyParts[1];
-                    if (!dependencyType) dependencyType = 'x';
+            var moduleLocation = self.resolve(moduleDescriptor.lookup, previousPath),
+                loadedModule = require(moduleLocation);
 
-                    return loader[dependencyType](dependencyAlias);
-                }),
-
-                path = moduleDescriptor.lookup ? moduleDescriptor.lookup : alias,
-                moduleLocation = self.resolve(path),
-                loadedModule = require(moduleLocation).apply(null, dependencies);
-
-            if (moduleDescriptor.singleton) config.singletons[alias] = loadedModule;
+            if (moduleDescriptor.deps) {
+                dependencies = moduleDescriptor.deps.map(function(item) {
+                    return self.load(item, moduleLocation);
+                });
+                loadedModule = loadedModule.apply(null, dependencies);
+                if (moduleDescriptor.singleton) config.singletons[alias] = loadedModule;
+            }
 
             return loadedModule;
         };
@@ -57,4 +47,3 @@ module.exports = function(config) {
 
     return new Din();
 };
-

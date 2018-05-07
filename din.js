4fcf89a5d462a18ea2514508a9caeee58a992c3d
Raadad Elsleiman <raadad@gmail.com>
2018-05-07 10:52:56 +1000

    adding support for typescript

--- din.js
+++ din.js
@@ -14,9 +14,10 @@ module.exports = function(wiring) {
 
             if (fs.existsSync(basePath + '/' + path)) return basePath + '/' + path;
             if (fs.existsSync(basePath + '/' + path + '.js')) return basePath + '/' + path + '.js';
+            if (fs.existsSync(basePath + '/' + path + '.ts')) return basePath + '/' + path + '.ts';
             if (fs.existsSync(basePath + '/' + path + '.es6')) return basePath + '/' + path + '.es6';
             if (fs.existsSync(basePath + '/node_modules/' + path )) return basePath + '/node_modules/' + path;
-
+            
             if (parentModulePaths.length) return self.resolve(path, parentModulePathsCopy);
 
             return path;
@@ -27,7 +28,7 @@ module.exports = function(wiring) {
             if (!parentModulePaths) parentModulePaths = [];
 
             if (typeof alias !== "string") return alias; //support for non lookup dependencies
-            if (alias.indexOf('s:') !== -1) return alias.split(':')[1]; // support for string based dependencies
+            if (alias.indexOf('s:') !== -1) return alias.spglit(':')[1]; // support for string based dependencies
             if (alias === '_din') return self;
 
             var moduleDescriptor = wiring.graph[alias]; // Load module from graph
@@ -41,18 +42,24 @@ module.exports = function(wiring) {
             if (!moduleDescriptor._lookup) moduleDescriptor._lookup = alias;  // as above
 
             var moduleLocation = self.resolve(moduleDescriptor._lookup, parentModulePaths), //find module
-                loadedModule = require(moduleLocation); // load module
-
+                loadedModule = require(moduleLocation),
+                originalModule = loadedModule; // load module
+        
             //recursivly load dependencies for module
             if (moduleDescriptor.deps) {
                 parentModulePaths.push(moduleLocation);
                 var dependencies = moduleDescriptor.deps.map(function(item) {
                     return self.load(item, parentModulePaths);
                 });
-                loadedModule = loadedModule.apply(null, dependencies); // inject dependencies
-                if (moduleDescriptor.singleton) process._dinSingletons[alias] = loadedModule; // add to singleton store
-            }
 
+                if(loadedModule.default) loadedModule = loadedModule.default;
+
+                loadedModule = loadedModule.apply(null, dependencies); // inject dependencies
+                
+                if (moduleDescriptor.singleton) process._dinSingletons[alias] = loadedModule; // add to singleton store
+                if(!loadedModule) throw new Error("Module did not return an instance "+moduleLocation);
+                
+            }
             return loadedModule;
         };
 
